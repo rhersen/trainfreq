b9f26676396ad0270be5b8d8cc983ca29078c67d
Rudolf Hersén <rudolf.hersen@schibsted.com>
2023-08-25 10:10:34 +0200

    fetch data from trafikverket.se

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,3 +1,57 @@
+import { error } from '@sveltejs/kit';
+
+// noinspection JSUnusedGlobalSymbols
 export async function load({ params }) {
-	return { s: JSON.stringify(params) };
+	const r = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
+		method: 'POST',
+		body: getBody(),
+		headers: {
+			'Content-Type': 'application/xml',
+			Accept: 'application/json'
+		}
+	});
+	if (!r.ok) throw error(r.status, r.statusText);
+
+	const { RESPONSE } = await r.json();
+	const [announcements] = RESPONSE.RESULT;
+	return {
+		params,
+		announcements: announcements.TrainAnnouncement,
+	};
+}
+
+function getBody() {
+	const now = Date.now();
+	const since = new Date(now - 30 * 6e4).toISOString();
+	const until = new Date(now + 24 * 60 * 6e4).toISOString();
+	return `
+<REQUEST>
+  <LOGIN authenticationkey='${process.env.TRAFIKVERKET_API_KEY}' />
+     <QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation' sseurl='false' schemaversion='1.6'>
+      <FILTER>
+         <AND>
+            <NE name='Canceled' value='true' />
+            <EQ name='Advertised' value='true' />
+            <EQ name='ActivityType' value='Avgang' />
+            <EQ name='LocationSignature' value='Tul' />
+        	<LIKE name='AdvertisedTrainIdent' value='/[02468]$/' />
+            <OR>
+               <GT name='AdvertisedTimeAtLocation' value='${since}' />
+               <GT name='EstimatedTimeAtLocation' value='${since}' />
+            </OR>
+            <LT name='AdvertisedTimeAtLocation' value='${until}' />
+         </AND>
+      </FILTER>
+      <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
+      <INCLUDE>AdvertisedTrainIdent</INCLUDE>
+      <INCLUDE>Deviation</INCLUDE>
+      <INCLUDE>EstimatedTimeAtLocation</INCLUDE>
+      <INCLUDE>FromLocation</INCLUDE>
+      <INCLUDE>ProductInformation</INCLUDE>
+      <INCLUDE>TimeAtLocation</INCLUDE>
+      <INCLUDE>TimeAtLocationWithSeconds</INCLUDE>
+      <INCLUDE>ToLocation</INCLUDE>
+      <INCLUDE>TrackAtLocation</INCLUDE>
+     </QUERY>
+</REQUEST>`;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
-    export let data;
+	export let data;
 </script>
 
 <h1>trainfreq</h1>
-<p>{data.s}</p>
+
+<ol>
+	{#each data.announcements as { AdvertisedTimeAtLocation, ToLocation }}
+		<li>{AdvertisedTimeAtLocation.substring(5, 16)} {ToLocation[0].LocationName}</li>
+	{/each}
+</ol>
+<p>{data.announcements}</p>
